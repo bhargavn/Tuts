b14792ed43b412e7500b0da4e8dd45f2356c9891
bhargavn <nilagiribhargav@gmail.com>
2015-08-05 17:39:28 +0530

    modified to cosider hashlist

--- Strings/LargestPalindrome.java
+++ Strings/LargestPalindrome.java
@@ -1,21 +1,28 @@
 package Strings;
 
+import java.util.HashMap;
+
 public class LargestPalindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s ="abcccbs";
-		int i=findPal(s);
+		HashMap<String, Integer>h=new HashMap<String,Integer>();
+		int i=findPal(s,h);
 System.out.println(i);
 	}
 
-	private static int findPal(String s) {
+	private static int findPal(String s,HashMap<String, Integer>h) {
 		// TODO Auto-generated method stub
+		boolean eval=h.containsKey(s);
+		if(eval){
+			return h.get(s);
+		}
 		boolean check=checkPalidrome(s);
-		if(check==true){
+		if(check){
 			return s.length();
 		}else{
-			return Math.max(findPal(s.substring(1)),findPal(s.substring(0,s.length()-1)));
+			return Math.max(findPal(s.substring(1),h),findPal(s.substring(0,s.length()-1),h));
 		}
 	}
 
